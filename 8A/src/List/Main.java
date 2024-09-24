9a0867bedbd0ceb6898fdae449f408dc46915bf9
vishnu vardhan <vishnuvardhan1701@gmail.com>
2024-09-24 10:53:44 +0530

    Add files via upload

---
+++ 8A/src/List/Main.java
@@ -0,0 +1,14 @@
+package List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("banana", "apple", "orange", "kiwi");
+
+        Collections.sort(strings, (s1, s2) -> s2.compareTo(s1));
+
+        System.out.println(strings);
+    }
+}
